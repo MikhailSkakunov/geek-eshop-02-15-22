24ab0c0ce9c192de6af7bfe90008a3a2f7fe107a
Mikhail <skakunovmy@gmail.com>
2022-04-08 16:23:36 +0300

    Commit_08

---
+++ spring-cloud-gateway/src/test/java/ru/geekbrains/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
